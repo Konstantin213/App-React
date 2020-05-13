4e5eac5b5465c6a693dd044b7abf3eb8e87be4b1
Konstantin213 <evroberkut@gmail.com>
2020-05-13 19:01:47 +0700

    Updates

---
+++ precache-manifest.d2090f9348ba4aecac457f82b601fd4f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c00f78d4caa77c2b6164fd7792f51e5",
+    "url": "/App-React/index.html"
+  },
+  {
+    "revision": "9426b5733cb71b5c5cf2",
+    "url": "/App-React/static/css/3.f36f7544.chunk.css"
+  },
+  {
+    "revision": "bcf01a3a9032683f5c95",
+    "url": "/App-React/static/css/4.6206a645.chunk.css"
+  },
+  {
+    "revision": "0b1e1df5cebc14f34bd4",
+    "url": "/App-React/static/css/main.5dd5eef9.chunk.css"
+  },
+  {
+    "revision": "831e26eefef2f3e23fc4",
+    "url": "/App-React/static/js/2.c346d37c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/App-React/static/js/2.c346d37c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9426b5733cb71b5c5cf2",
+    "url": "/App-React/static/js/3.00adb561.chunk.js"
+  },
+  {
+    "revision": "bcf01a3a9032683f5c95",
+    "url": "/App-React/static/js/4.936a6810.chunk.js"
+  },
+  {
+    "revision": "0b1e1df5cebc14f34bd4",
+    "url": "/App-React/static/js/main.15dd9170.chunk.js"
+  },
+  {
+    "revision": "406a016c76ec01eb2daf",
+    "url": "/App-React/static/js/runtime-main.07626557.js"
+  },
+  {
+    "revision": "ee773c8f83a83a805f3e02b7b8df0131",
+    "url": "/App-React/static/media/preloader.ee773c8f.svg"
+  },
+  {
+    "revision": "42ecc3876bc7855b13b29cb2db22fb5c",
+    "url": "/App-React/static/media/user.42ecc387.jpg"
+  }
+]);
